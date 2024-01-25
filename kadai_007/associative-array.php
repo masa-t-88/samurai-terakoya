3c667528718a9de32896ddcb7f3f5d5cc89a0bae
masa-t-88 <156871474+masa-t-88@users.noreply.github.com>
2024-01-25 17:07:13 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題</title>
+</head>
+<body>
+
+<p>
+    <?php
+      $vegetable = [
+        'name' => 'onion',
+        'price' => 200,
+        'weight' => 160
+      ];
+      print_r($vegetable);
+    ?>
+</p>
+    
+</body>
+</html>
